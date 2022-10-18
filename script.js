592b3105825e478c959114db2abfa80e8bea55ae
Kendall Coleman <kendallcoleman@Kendalls-MacBook-Air.local>
2022-10-18 19:39:15 -0400

    started

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Today's a Great Day to be a Wildcat!")
+
+//  1. Prompt the user for the password criteria
+//      a. Password length 8 < 128
+//      b. Lowercase, uppercase, numbers, & special charachters
+
+
+
+
+
+
+
+//  2. Validate the input
+
+
+
+
+
+
+
+
+//  3. Generate password based on criteria
+  
+  
+  
+  
+  
+  
+// Displayed password on the page
+  return "Generated Password Goes Here"
+}
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
