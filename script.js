30e02ce73ac7e047783edfae214355d362412922
Kendall Coleman <kendallcoleman@Kendalls-MacBook-Air.local>
2022-10-27 20:40:42 -0400

    trial run

--- script.js
+++ script.js
@@ -2,15 +2,42 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  console.log("Today's a Great Day to be a Wildcat!")
+  console.log("Today's a Great Day to be a Wildcat!");
 
 //  1. Prompt the user for the password criteria
 //      a. Password length 8 < 128
 //      b. Lowercase, uppercase, numbers, & special charachters
 
+var userInput = window.prompt("Want to create a password?")
+
+if (userInput === true) {
+  console.log("Yes")
+  return
+}
+
+var passwordlength = parseInt(userInput)
+
+if (isNaN(passwordlength)) {
+  window.alert("What you have entered is not a number!")
+  return
+}
+
+if (isNaN(passwordlength < 8 || passwordlength > 128)) {
+  window.alert("Keep password length between 8 and 128 characters!")
+  return
+}
+
+var userWantsNumbers = window.confirm("Do you want numbers in your password?")
+
+var lowercaseLtrs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseLtrs = []
+var symbols = ["!","@", "#","$","%","^","&","*","?"]
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
 
-
+for (var i =0; i < lowercaseLtrs.length; i++) {
+  uppercaseLtrs[i] = lowercaseLtrs[i].toUpperCase()
+}
 
 
 
