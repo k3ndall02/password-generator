8284f4c2231eff6a394bd3e5f23f0e880f19e34e
Kendall Coleman <kendallcoleman@Kendalls-MacBook-Air.local>
2022-10-31 23:18:45 -0400

    uppercase letter

--- script.js
+++ script.js
@@ -35,13 +35,13 @@ var userWantsUppercaseLtrs = window.confirm("Do you want to use lowercase letter
 
 
 var lowercaseLtrs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercaseLtrs = []
+var uppercaseLtrs = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var symbols = ["!","@", "#","$","%","^","&","*","?"]
 var numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
 
 for (var i = 0; i < lowercaseLtrs.length; i++) {
-  uppercaseLtrs[i] = lowercaseLtrs[i].toUpperCase()
+  console.log(lowercaseLtrs)
 }
 
 for (var i = 0; i < symbols.length; i++) {
@@ -50,6 +50,10 @@ for (var i = 0; i < symbols.length; i++) {
 
 for (var i = 0; i < numbers.length; i++) {
   console.log(numbers)
+}
+
+for (var i = 0; i < uppercaseLtrs.length; i++) {
+  console.log(uppercaseLtrs)
 }
 
 //  2. Validate the input
